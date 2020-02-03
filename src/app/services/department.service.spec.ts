6acc15c0429949000b5074aae3a1eb1a9cb0c248
Harry Dbrandy <devdbrandy@gmail.com>
2020-02-03 12:30:09 +0100

    ✅ add tests for services

--- src/app/services/department.service.spec.ts
+++ src/app/services/department.service.spec.ts
@@ -1,12 +1,68 @@
-import { TestBed } from '@angular/core/testing';
-
 import { DepartmentService } from './department.service';
+import { Manager, Developer, Department } from '../shared/models';
 
 describe('DepartmentService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: DepartmentService;
+
+  beforeEach(() => {
+    service = new DepartmentService();
+  });
 
   it('should be created', () => {
-    const service: DepartmentService = TestBed.get(DepartmentService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get all departments', () => {
+    service.getDepartments().subscribe(value => {
+      expect(value).toEqual([]);
+    });
+  });
+
+  it('should get department by name', () => {
+    const manager = new Manager('Clark Kent');
+    const departmentData = { name: 'TDD', manager };
+    service.departments.push(departmentData);
+
+    service.getDepartmentByName(departmentData.name).subscribe(value => {
+      expect(value.name).toEqual(departmentData.name);
+    });
+  });
+
+  it('should create new department', () => {
+    const manager = new Manager('Martin Luton');
+    const data = { name: 'Operations', manager };
+    service.addDepartment(data).subscribe(value => {
+      expect(value.name).toEqual('Operations');
+      expect(value.manager.name).toEqual('Martin Luton');
+      expect(value.manager.role).toEqual('manager');
+      expect(value.manager.allocation).toEqual(30000);
+    });
+  });
+
+  it('should add member to existing department', () => {
+    const developer = new Developer('Harry Pots');
+    const manager = new Manager('Clark Kent');
+    const department = new Department('Developers', manager);
+
+    service.departments.push(department);
+    service.addMemberToDepartment(developer, department).subscribe(value => {
+      expect(value).toBeTruthy();
+    });
+  });
+
+  it('should remove member from existing department', () => {
+    const developer = new Developer('Harry Pots');
+    const manager = new Manager('Clark Kent');
+    const department = new Department('Developers', manager);
+
+    service.departments.push(department);
+
+    service.removeMemberFromDepartment(developer, department).subscribe(value => {
+      expect(value).toBeTruthy();
+
+      const departmentIndex = service.departments.indexOf(department);
+      expect(service.departments[departmentIndex].manager.team.indexOf(developer))
+        .toEqual(-1);
+    });
   });
 });
--- src/app/services/employee.service.spec.ts
+++ src/app/services/employee.service.spec.ts
@@ -1,12 +1,46 @@
-import { TestBed } from '@angular/core/testing';
-
 import { EmployeeService } from './employee.service';
+import { Developer, Manager } from '../shared/models';
 
 describe('EmployeeService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: EmployeeService;
+  let developer = new Developer('Johnny Bravo');
+  let manager = new Manager('King David');
+
+  beforeEach(() => {
+    developer = new Developer('Johnny Bravo');
+    manager = new Manager('King David');
+
+    service = new EmployeeService();
+    service.employees.push(...[manager, developer]);
+  });
 
   it('should be created', () => {
-    const service: EmployeeService = TestBed.get(EmployeeService);
     expect(service).toBeTruthy();
+  });
+
+  it('should get all departments', () => {
+    service.getEmployees().subscribe(value => {
+      expect(value.length).toEqual(2);
+    });
+  });
+
+  it('should create a new employee', () => {
+    service.createEmployee('Mary Jane', 'manager').subscribe(value => {
+      expect(value.name).toEqual('Mary Jane');
+      expect(value.role).toEqual('manager');
+      expect(value.allocation).toEqual(30000);
+    });
+  });
+
+  it('should assign employee\'s manager', () => {
+    service.setManager(developer, manager).subscribe(value => {
+      expect(value.hasManager).toBeTruthy();
+    });
+  });
+
+  it('should remove employee from manager teams', () => {
+    service.removeManager(developer).subscribe(value => {
+      expect(value.hasManager).toBeFalsy();
+    });
   });
 });
